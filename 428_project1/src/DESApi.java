19e88d141735240b14c2fb71313e958e253ab5de
Landon <landon.reams.13@cnu.edu>
2016-02-08 14:45:50 -0500

    Added solution to Question 2.
    Hint: The answer is CPSC 428.

--- 428_project1/src/DESApi.java
+++ 428_project1/src/DESApi.java
@@ -1,6 +1,10 @@
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
+/*
+ * Question 1: Ciphertext is [E5, 0E, A6, 1E, 50, A3, D2, 69]
+ * Question 2: Plaintext  is "CPSC 428"
+ */
 
 public class DESApi {
 	// Useful method
@@ -66,10 +70,24 @@ public class DESApi {
 	/*
 	 * DES Decryption
 	 */
-	public static byte[] DES_Decrypt(byte[] ciphertext, byte[] key){
-		byte[] plaintext = null;
-		//decrypt
-		return plaintext;
+	public static String DES_Decrypt(byte[] ciphertext, byte[] keyBytes){
+		Cipher cipher;
+		SecretKeySpec key = new SecretKeySpec(keyBytes, "DES");;
+		byte [] plainttextByte = null;
+		
+		try
+		{
+			
+			cipher = Cipher.getInstance("DES/ECB/NoPadding");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			
+			plainttextByte = cipher.doFinal(ciphertext);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return new String(plainttextByte);
 	}
 	
 	/*
@@ -85,8 +103,15 @@ public class DESApi {
 	public static void main(String[] args){
 		// Question 1 
 		String myPlaintText = "Dee Bugg";
-		System.out.println("CipherText is: ");
+		System.out.print("CipherText is: ");
 		printByteArray(DES_Encrypt(myPlaintText));
+		
+		//Question 2
+		byte[] cipher2 = { (byte) 0x9D, (byte) 0x1C, (byte) 0x1D, (byte) 0x94, (byte) 0x8F, (byte) 0x21, (byte) 0x55, (byte) 0xC5};
+		byte[] key2    = { (byte) 0x46, (byte) 0xAA, (byte) 0x20, (byte) 0x1E, (byte) 0xF4, (byte) 0x3C, (byte) 0x92, (byte) 0xD2};
+		String plain2  = DES_Decrypt(cipher2, key2);
+		System.out.println("Question 2: Plaintext is "+plain2);
+		
 	}
 	
 }
