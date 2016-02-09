fc6e25eca22c25c1598135968752548b7ee4c373
LANDON <landon.reams.13@cnu.edu>
2016-02-09 00:39:11 -0500

    Found all correct answers! Took six hours though.

--- 428_project1/src/DESApi.java
+++ 428_project1/src/DESApi.java
@@ -6,7 +6,18 @@ import javax.crypto.spec.SecretKeySpec;
 /*
  * Question 1: Ciphertext is [E5, 0E, A6, 1E, 50, A3, D2, 69]
  * Question 2: Plaintext  is "CPSC 428"
- * Question 3: Most recent guess was --k: [90, 4E, F2, CC, 04, 3C, AE, 2F]
+ * Question 3: Full key is [90, 4E, F2, CC, 86, 02, 4A, 16]
+ * Question 4: There are several keys that lead to "Good Job":
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D4, C6, 9E]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D4, C6, 9F]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D4, C7, 9E]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D4, C7, 9F]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D5, C6, 9E]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D5, C6, 9F]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D5, C7, 9E]
+ * p: Good Job |k: [BA, 54, 68, 08, 12, D5, C7, 9F]
+ * It is impossible to tell which the original key was, but as there are so few to test,
+ * it isn't significant.
  */
 
 public class DESApi {
@@ -25,19 +36,24 @@ public class DESApi {
 	}
 
 	/*
-	 * Print out a byte array in hex format
+	 * Generates a String from a byte array
 	 */
-	public static void printByteArray(byte [] array)
-	    {
-		System.out.print("[");
-		for(int i = 0; i < array.length-1; i++)
-		{
-		    System.out.print(Integer.toHexString((array[i]>>4)&0x0F).toUpperCase());
-		    System.out.print(Integer.toHexString(array[i]&0x0F).toUpperCase() + ", ");
+	public static String byteArrayToString(byte[] array) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		for (int i = 0; i < array.length - 1; i++) {
+			sb.append(Integer.toHexString((array[i] >> 4) & 0x0F)
+					.toUpperCase());
+			sb.append(Integer.toHexString(array[i] & 0x0F).toUpperCase()
+					+ ", ");
 		}
-		System.out.print(Integer.toHexString(array[array.length-1]>>4&0x0F).toUpperCase());
-		System.out.println(Integer.toHexString(array[array.length-1]&0x0F).toUpperCase() + "]");
-	    }
+		sb.append(Integer.toHexString(
+				array[array.length - 1] >> 4 & 0x0F).toUpperCase());
+		sb.append(Integer.toHexString(array[array.length - 1] & 0x0F)
+				.toUpperCase() + "]");
+		
+		return sb.toString();
+	}
 	
 	/*
 	 * DES Encryption
@@ -103,13 +119,19 @@ public class DESApi {
 		}
 		//Finding key
 		
+		int c = 0;
+		
 		byte[] plaintextGuess = new byte[plaintext.length];
 		while(!assertByteArraysEqual(plaintext, plaintextGuess)){
 			plaintextGuess = DES_Decrypt(ciphertext, fullKey);
-			System.out.print("--k: ");
-			printByteArray(fullKey);
+			if(c % 10000 == 0){
+				System.out.print("--k: ");
+				byteArrayToString(fullKey);
+			}
 			if(!assertByteArraysEqual(plaintext, plaintextGuess))
 				incrementByteArrayByOne(fullKey);
+			
+			++c;
 		}
 		return fullKey;
 	}
@@ -128,8 +150,12 @@ public class DESApi {
 		
 		for(int i = 0; i < iterations; ++i){
 			plaintext = DES_Decrypt(ciphertext, key);
+			if(i % 100000 == 0){
+				System.out.println("--k: " +	byteArrayToString(key));
+			
+			}
 			if(isOnlyAscii(plaintext)){
-				result.add("p: " + new String(plaintext) + " |k: " + new String(key));
+				result.add("p: " + new String(plaintext) + " |k: " + byteArrayToString(key));
 			}
 			incrementByteArrayByOne(key);
 		}
@@ -152,39 +178,43 @@ public class DESApi {
 	private static boolean isOnlyAscii(byte[] array){
 		for(byte b : array){
 			//If is whitespace...
-			if (b == (byte) 0x20)
-				return true;
-			//Or uppercse...
-			if (b >= (byte) 0x41 && b <= (byte) 0x5A)
-				return true;
-			//Or lowercase...
-			if(b >= (byte) 0x61 && b <= (byte)0x7A)
-				return true;
+			if(!Character.isLetter((char)b) && !Character.isWhitespace((char)b)){
+				return false;
+			}
+			if((char) b > 'z') return false;
 		}
-		return false;
+		return true;
 	}
 	
 	public static void main(String[] args){
-		// Question 1 
-		String myPlaintText = "Dee Bugg";
-		System.out.print("Question 1: Ciphertext is ");
-		printByteArray(DES_Encrypt(myPlaintText));
-		
-		//Question 2
-		byte[] cipher2 = { (byte) 0x9D, (byte) 0x1C, (byte) 0x1D, (byte) 0x94, (byte) 0x8F, (byte) 0x21, (byte) 0x55, (byte) 0xC5};
-		byte[] key2    = { (byte) 0x46, (byte) 0xAA, (byte) 0x20, (byte) 0x1E, (byte) 0xF4, (byte) 0x3C, (byte) 0x92, (byte) 0xD2};
-		String plain2  = new String(DES_Decrypt(cipher2, key2));
-		System.out.println("Question 2: Plaintext is "+plain2);
-		
-		//Question 3
-		byte[] cipher3 = { (byte) 0xA5, (byte) 0x99, (byte) 0x04, (byte) 0x72, (byte) 0x39, (byte) 0x95, (byte) 0x41, (byte) 0xEC};
-		byte[] partialKey = { (byte) 0x90, (byte) 0x4E, (byte) 0xF2, (byte) 0xCC};
-		String plaintext = "Captains";
-		byte[] fullKey = DES_FindingKey(plaintext.getBytes(), cipher3, partialKey);
-		System.out.print("Question 3: Full key is: ");
-		printByteArray(fullKey);
-		
+//		// Question 1 
+//		String myPlaintText = "Dee Bugg";
+//		System.out.print("Question 1: Ciphertext is ");
+//		System.out.println(byteArrayToString(DES_Encrypt(myPlaintText)));
+//		
+//		//Question 2
+//		byte[] cipher2 = { (byte) 0x9D, (byte) 0x1C, (byte) 0x1D, (byte) 0x94, (byte) 0x8F, (byte) 0x21, (byte) 0x55, (byte) 0xC5};
+//		byte[] key2    = { (byte) 0x46, (byte) 0xAA, (byte) 0x20, (byte) 0x1E, (byte) 0xF4, (byte) 0x3C, (byte) 0x92, (byte) 0xD2};
+//		String plain2  = new String(DES_Decrypt(cipher2, key2));
+//		System.out.println("Question 2: Plaintext is "+plain2);
+//		
+//		//Question 3
+//		byte[] cipher3 = { (byte) 0xA5, (byte) 0x99, (byte) 0x04, (byte) 0x72, (byte) 0x39, (byte) 0x95, (byte) 0x41, (byte) 0xEC};
+//		byte[] partialKey = { (byte) 0x90, (byte) 0x4E, (byte) 0xF2, (byte) 0xCC, (byte) 0x04, (byte) 0x3C, (byte) 0xAE, (byte) 0x2F};
+//		String plaintext = "Captains";
+//		byte[] fullKey = DES_FindingKey(plaintext.getBytes(), cipher3, partialKey);
+//		System.out.print("Question 3: Full key is: ");
+//		System.out.println(byteArrayToString(fullKey));
+//		
 		//Question 4
+//		byte[] cipher4 = { (byte) 0xB1, (byte) 0x80, (byte) 0xE8, (byte) 0x05, (byte) 0x4E, (byte) 0x7D, (byte) 0xD6, (byte) 0x4C};
+//		byte[] partKey = { (byte) 0xBA, (byte) 0x54, (byte) 0x68, (byte) 0x08, (byte) 0x12};
+//		ArrayList<String> results = DES_guessPlaintext(cipher4, partKey, 16581375);
+//		for(String s : results){
+//			System.out.println(s);
+//		}
 	}
+	
+	
 	
 }
